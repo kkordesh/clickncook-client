8300fcd11b463a7f843fbbdbcede32a022412fe0
Kimberly Long <kimberlylong2262@gmail.com>
2022-02-16 00:24:13 -0500

    added home page & linked with navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navbar from './Home/Navbar';
 import './App.css';
+import Home from './Home/Home'
 
 function App() {
   return (
     <div className='App'>
         <Navbar />
+        <Home />
     </div>
   );
   };
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import '../App.css'
+import { Button } from './Button';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div
-            style={{
-                fontSize: '2rem',
-                display: 'flex',
-                justifyContent: 'center',
-                marginTop: '2rem',
-            }}
-        >
-            Home Page
+        <div className='home-container'>
+            <img src="../assets/dilyara-garifullina-5fn1LeWzhX4-unsplash.jpg" />
+            <h1>Storage for all  your special recipes</h1>
+                <Button 
+                    className='btns' 
+                    buttonStyle='btn--outline' 
+                    buttonSize='btn--large'>
+                       Get Started
+                </Button>
         </div>
     )
 }
